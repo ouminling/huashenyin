a6d94d2065bdd3045f460ebbbbbb6749772e9be4
花神引 <64060532+ouminling@users.noreply.github.com>
2022-03-31 13:14:05 +0800

    修改端口为8081

--- huashenyin-ui/vue.config.js
+++ huashenyin-ui/vue.config.js
@@ -37,7 +37,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:8080`,
+        target: `http://localhost:8081`,
         changeOrigin: true,
         //pathRewrite:路径重写
         pathRewrite: {
